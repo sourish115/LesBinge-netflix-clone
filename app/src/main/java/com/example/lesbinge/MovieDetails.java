6afbb20eaaa21e450f5d30de92dea158856c5d35
sourish ghosh <sourish115@live.com>
2021-01-23 21:06:43 +0530

    UI fixes and added cover image animation

--- app/src/main/java/com/example/lesbinge/MovieDetails.java
+++ app/src/main/java/com/example/lesbinge/MovieDetails.java
@@ -3,14 +3,17 @@ package com.example.lesbinge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MovieDetails extends AppCompatActivity {
     private ImageView movieImg, coverImage;
     private TextView movieTitleTv, movieDescTv;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +29,16 @@ public class MovieDetails extends AppCompatActivity {
         int imgURL = getIntent().getExtras().getInt("imgURL");
         int coverImageUrl = getIntent().getExtras().getInt("coverImg");
         movieImg = findViewById(R.id.movie_img);
-        coverImage = findViewById(R.id.imageView);
+        coverImage = findViewById(R.id.detail_movie_cover);
         movieTitleTv = findViewById(R.id.detail_movie_title);
         movieDescTv = findViewById(R.id.detail_movie_desc);
+        fab = findViewById(R.id.play_fab);
         Glide.with(this).load(imgURL).into(movieImg);
         Glide.with(this).load(coverImageUrl).into(coverImage);
         movieTitleTv.setText(movieTitle);
         getSupportActionBar().setTitle(movieTitle);
+        coverImage.setAnimation(AnimationUtils.loadAnimation(this,R.anim.click_animation));
+        fab.setAnimation(AnimationUtils.loadAnimation(this,R.anim.click_animation));
 
     }
 }

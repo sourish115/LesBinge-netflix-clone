208e7b19516afc1d8eb8f98494032d653221016a
sourish ghosh <sourish115@live.com>
2021-01-24 23:41:46 +0530

    added transition animation for login, ui changes

--- app/src/main/java/com/example/lesbinge/Splash.java
+++ app/src/main/java/com/example/lesbinge/Splash.java
@@ -2,24 +2,31 @@ package com.example.lesbinge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        logo = findViewById(R.id.imageView);
+
         Handler handler = new Handler(Looper.myLooper());
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent i = new Intent(getApplicationContext(),Login.class);
-
+                //i.putExtra("imgURL",R.drawable.splash);
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Splash.this,logo, "sharedNameLogo");
+                startActivity(i,options.toBundle());
             }
         },2000);
     }

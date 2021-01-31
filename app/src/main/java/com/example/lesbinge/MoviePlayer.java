cde82dce9151020eff7d88ca401bb80ec651ece6
sourish ghosh <sourish115@live.com>
2021-01-31 23:26:50 +0530

    Added exo player

--- app/src/main/java/com/example/lesbinge/MoviePlayer.java
+++ app/src/main/java/com/example/lesbinge/MoviePlayer.java
@@ -2,13 +2,47 @@ package com.example.lesbinge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
 public class MoviePlayer extends AppCompatActivity {
+    private PlayerView playerView;
+    private SimpleExoPlayer simpleExoPlayer;
+    public static final String VID_URL="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_player);
+
+        initMoviePlayer();
+
+    }
+
+    private void initMoviePlayer(){
+        playerView=findViewById(R.id.movie_player);
+        simpleExoPlayer= ExoPlayerFactory.newSimpleInstance(getApplicationContext());
+        playerView.setPlayer(simpleExoPlayer);
+        DataSource.Factory dataSource = new DefaultDataSourceFactory(getApplicationContext(),
+                Util.getUserAgent(this, "appname"));
+        MediaSource videoSource = new ExtractorMediaSource.Factory(dataSource).createMediaSource(Uri.parse(VID_URL));
+        simpleExoPlayer.prepare(videoSource);
+        simpleExoPlayer.setPlayWhenReady(true);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        simpleExoPlayer.release();
     }
 }
